88350baa22c5c8ee5f9efcbe2131f9585670d38b
Macha Celeste <shetah1@yahoo.com>
2025-05-01 11:18:33 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace BasedDataPlugin;
+
+[BepInPlugin("com.machaceleste.baseddataplugin", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var harmony = new Harmony("com.machaceleste.baseddataplugin");
+        harmony.PatchAll();
+    }
+}
---
+++ patches/DatabasePatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+[HarmonyPatch]
+public class DatabasePatch
+{
+    [HarmonyPatch(typeof(Database), "GetContenido")]
+    class GetContenidoPatch
+    {
+        static void Postfix(ref string __result)
+        {
+            if (!string.IsNullOrEmpty(__result))
+            {
+                try
+                {
+                    JsonConvert.DeserializeObject<Library>(__result);
+                }
+                catch (Exception ex)
+                {
+                    __result = Encoding.UTF8.GetString(Convert.FromBase64String(__result));
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Database), "SetContenido")]
+    class SetContenidoPatch
+    {
+        static void Prefix(ref FileSystem.Archivo file, ref string contenido, ref string prevID)
+        {
+            contenido = Convert.ToBase64String(Encoding.UTF8.GetBytes(contenido));
+        }
+    }
+}
